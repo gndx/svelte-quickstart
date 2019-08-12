34a341accebb6c2d518b6166d052e8feedc50e0a
Oscar Barajas Tavares <oscar@arepa.dev>
2019-08-12 00:08:38 -0500

    [UPDATE] Svelte Fetch App

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,38 @@
 <script>
-  let name = "Mundo";
+  import { onMount } from "svelte";
+
+  const API = "https://rickandmortyapi.com/api/character";
+  let data = [];
+  let characters = [];
+
+  onMount(async () => {
+    const res = await fetch(API);
+    data = await res.json();
+    characters = data.results;
+  });
 </script>
 
-<h1>Hola {name}!</h1>
+<style>
+  .characters {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 8px;
+  }
+  figure,
+  img {
+    width: 100%;
+    margin: 0;
+  }
+</style>
+
+<div class="characters">
+  {#each characters as character}
+    <figure>
+      <img src={character.image} alt={character.name} />
+      <figcaption>{character.name}</figcaption>
+    </figure>
+  {:else}
+    <p>loading...</p>
+  {/each}
+</div>
